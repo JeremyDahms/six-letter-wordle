1c460106f419826d0e0716c0f62063b28b5dba6e
Jeremy Dahms <jdahms@vertafore.com>
2022-01-25 18:10:36 -0700

    Add gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -72,6 +72,11 @@ module.exports = {
 };
 
 if (process.env.NODE_ENV === "production") {
+  module.exports.output = {
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/six-letter-wordle/",
+    filename: "build.js"
+  };
   module.exports.devtool = "#source-map";
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
